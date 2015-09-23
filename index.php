a3e158efbf0183b0d065af9684e782d03fc5de34
Priyesh Mehta <priyesh.mehta@infobeans>
2015-09-23 16:16:19 +0530

    Header contact text changed to italics.

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="contact_us.php">Contact</a></li>
+            <li><a href="contact_us.php"><i>Contact</i></a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
