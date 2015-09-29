e639c4754a0a6d1a9d3cfe9b9ddb75e5cd0c69a1
hiteshinfobeans <hitesh.agrawal@infobeans.com>
2015-09-29 18:12:36 +0530

    Content added

--- about_us.php
+++ about_us.php
@@ -60,6 +60,9 @@
         <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis dictum lectus, eget pretium lacus. Donec hendrerit, velit in fermentum pretium, neque erat fermentum lorem, mattis efficitur tortor lectus at nunc. Sed feugiat, diam lobortis ultrices elementum, leo neque imperdiet elit, nec tristique nunc lorem at purus. Aenean nulla augue, eleifend ut ornare quis, lacinia sed felis. Vivamus at accumsan est. Proin feugiat orci in ligula laoreet fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur luctus commodo risus et molestie. Etiam commodo eros justo, in malesuada risus iaculis at.</p>
 
 	<p class="lead">Pellentesque euismod fermentum nisi sagittis cursus. Ut sed congue purus. Sed facilisis sodales sapien, nec euismod arcu finibus at. Fusce id dignissim nisi, quis porttitor nunc. Etiam at nulla id lectus tempor tempor. Aliquam libero dui, pharetra a bibendum ac, lobortis non erat. Nam at commodo erat, quis imperdiet tellus. Vivamus convallis vel augue et egestas. Cras nec ornare ipsum, nec porta diam. Curabitur porttitor lorem felis, eget imperdiet erat molestie placerat. Phasellus aliquet ante nec libero vehicula fermentum. Maecenas eget eros vitae ipsum aliquet vestibulum. Vestibulum at nibh vitae dolor hendrerit rutrum quis nec mi.</p>
+	<p class="lead">This is Code added by Hitesh Agrawal</p>
+
+
       </div>
 
     </div><!-- /.container -->
