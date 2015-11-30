b3eb8da1878cdc8d9a12ff0db8c9f061c07058e3
abhijeet.dange <abhijeet.dange@infobeans.com>
2015-11-30 14:27:51 +0530

    Added extra content on home page

--- index.php
+++ index.php
@@ -60,7 +60,8 @@
       <div class="starter-template">
         <h1>Bootstrap starter template</h1>
         <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
+     	<p>This content added by Rashmi</p>
+	 </div>
 
     </div><!-- /.container -->
 
